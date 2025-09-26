07b3309eb3c0c5ff5b5312289433659f114e95a9
gaurav chopra <chopragc03@gmail.com>
2025-09-26 13:01:54 +0530

    Add frontend dist folder for deployment

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url="http://localhost:4000";
+    const url="https://gc-test-gpa9brh8hxguexhd.centralindia-01.azurewebsites.net";
     
     //token
     const [token,setToken]=useState("");
