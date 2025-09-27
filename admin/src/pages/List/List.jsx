0bf35c1581bec075e9ea378646688a5de9052339
gaurav chopra <chopragc03@gmail.com>
2025-09-27 12:33:54 +0530

    Add frontend dist folder for deployment

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -7,7 +7,7 @@ const List = ({url}) => {
     const [list,setList]=useState([]);
 
     const fetchList= async ()=>{
-        const response =await axios.get("https://gc-test-gpa9brh8hxguexhd.centralindia-01.azurewebsites.net/api/food/list");
+        const response =await axios.get(`${url}/api/food/list`);
        
         if(response.data.success){
             setList(response.data.data);
